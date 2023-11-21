fd2e13fb4ad0089a8092f0570f06a7a4cc034b2b
Simon Slamka <51794014+simonSlamka@users.noreply.github.com>
2023-11-21 13:45:38 +0100

    superimposed detected landmarks on the input img; attempted using cv2's FaceMark, but did not work for now

--- landmarks_detector.py
+++ landmarks_detector.py
@@ -2,6 +2,7 @@ import cv2
 import dlib
 import numpy as np
 from utils import load_image, convert_to_gray, resize_image
+import logging
 
 class LandmarksDetector:
     def __init__(self, predictorPath):
@@ -22,9 +23,34 @@ class LandmarksDetector:
         #     raise ValueError('No landmarks detected in {}'.format(imagePath))
 
         # For each detected face, find the landmark.
-        for (i, rect) in enumerate(landmarks):
+        for _, rect in enumerate(landmarks):
             shape = self.predictor(gray, rect)
             shape = self._shape_to_np(shape)
+
+            # Draw bounding box for each face detected
+            x = rect.left()
+            y = rect.top()
+            w = rect.width()
+            h = rect.height()
+            cv2.rectangle(image, (x, y), (x+w, y+h), (255, 255, 0), 1)
+
+            # Estimate forehead
+            forehead_height = int(0.25 * h)
+            cv2.rectangle(image, (x, y - forehead_height), (x + w, y), (0, 255, 255), 1)
+
+            for (x, y) in shape:
+                x = int(x * image.shape[1] / gray.shape[1])
+                y = int(y * image.shape[0] / gray.shape[0])
+
+                if x < rect.left() or x > rect.right() or y < rect.top() or y > rect.bottom():
+                    cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
+                    logging.warning("Landmark outside of bounding box at coords ({}, {})".format(x, y))
+                else:
+                    cv2.circle(image, (x, y), 1, (255, 0, 255), -1)
+            cv2.namedWindow("out", cv2.WINDOW_KEEPRATIO)
+            cv2.imshow("out", image)
+            cv2.resizeWindow("out", 1024, 1024)
+            cv2.waitKey(0)
 
             return shape
 
